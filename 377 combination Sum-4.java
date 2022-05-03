64f248dada233a4022535a487b754822c1efbb32
susmita09 <59968268+susmita09@users.noreply.github.com>
2022-05-03 21:24:53 +0530

    Update 377 combination Sum-4.java

--- 377 combination Sum-4.java
+++ 377 combination Sum-4.java
@@ -1,3 +1,4 @@
+//memonization
 class Solution {
     //TC = 0(amount * coins) SC = 0(amount) DP+RCS
     
@@ -21,3 +22,19 @@ class Solution {
         return memo(nums,target,dp);
     }
 }
+
+ //tabulation
+
+     public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+         dp[0] = 1;
+         for(int i=1;i<=target;i++){
+             for(int j=0;j<nums.length;j++){
+                 if(i - nums[j] >= 0){
+                    dp[i] += dp[i-nums[j]];
+                 }
+            }
+         }
+         return dp[target];
+    }
+
